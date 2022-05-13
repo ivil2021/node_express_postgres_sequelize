7c73439c03919f63844e6380f34150211c28575d
ivil2021 <89508514326@mail.ru>
2022-05-13 16:16:16 +0300

    first commit

---
+++ migrations/20220513114411-create-news-list.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('NewsList', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      news_title: {
+        type: Sequelize.STRING,
+      },
+      news_text: {
+        type: Sequelize.STRING,
+      },
+      news_date: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('NewsList');
+  },
+};
---
+++ models/newslist.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class NewsList extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  NewsList.init({
+    news_title: DataTypes.STRING,
+    news_text: DataTypes.STRING,
+    news_date: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'NewsList',
+  });
+  return NewsList;
+};
