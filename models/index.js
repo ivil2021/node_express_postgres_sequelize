56c441cd967b00ec8ad29f1625d304f82e51446c
ivil2021 <89508514326@mail.ru>
2022-05-26 16:21:44 +0300

    fix: change connection

--- models/index.js
+++ models/index.js
@@ -8,19 +8,17 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
+// let sequelize;
 
-console.log(config)
-
-sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+// sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   config
+// );
 
 // const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
-// const sequelize = new Sequelize('postgres://xhegyxcvfkfbeg:a8977b67913c4f7a45d1e7cb1b4acfa9d85e364ba4e6929151f3923ae6715712@ec2-52-86-115-245.compute-1.amazonaws.com:5432/d2t21finm4qipn');
+const sequelize = new Sequelize('postgres://xhegyxcvfkfbeg:a8977b67913c4f7a45d1e7cb1b4acfa9d85e364ba4e6929151f3923ae6715712@ec2-52-86-115-245.compute-1.amazonaws.com:5432/d2t21finm4qipn');
 
 fs.readdirSync(__dirname)
   .filter((file) => {
