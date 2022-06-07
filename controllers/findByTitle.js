b770eb4fbf708a6af426ea2ffaf756bf9e04d048
ivil2021 <89508514326@mail.ru>
2022-06-07 18:24:18 +0300

    draft: modal windows works. Getting news by title entered into the input field works.

--- controllers/findByTitle.js
+++ controllers/findByTitle.js
@@ -6,6 +6,7 @@ const NewsList = require('../models').NewsList;
 
 const findByTitle = async (req, res) => {
   try {
+    // console.log('req from find by title back: ', req);
     // TODO: to delete after final check
     // const page = req.pagination.page;
     // const limit = req.pagination.limit;
@@ -29,41 +30,57 @@ const findByTitle = async (req, res) => {
     //   OFFSET ${req.pagination.offset};
     // `;
 
+    const title = req.query.title;
+    let requestedNews;
+    let result;
+    const filterCondition = title ? `WHERE "news_title" = '${title}'` : '';
+
+    // const query = `
+    // WITH DATA AS (
+    //   SELECT count(*) AS "count" FROM "NewsList"
+    //   GROUP BY "id"
+    // ) SELECT *
+    // FROM "NewsList"
+    // ${filterCondition}
+    // ORDER BY "NewsList"."id"
+    // LIMIT ${req.pagination.limit}
+    // OFFSET ${req.pagination.offset};
+    // `;
+
     const query = `
-    WITH DATA AS (
-      SELECT "id", COUNT(*) AS count FROM "NewsList" GROUP BY "id"
-    ) SELECT *
+    SELECT *
     FROM "NewsList"
+    ${filterCondition}
     ORDER BY "NewsList"."id"
     LIMIT ${req.pagination.limit}
     OFFSET ${req.pagination.offset};
     `;
 
-    const title = req.query.title;
-    let requestedNews;
-    let result;
+    const countQuery = `SELECT count(*) AS "count" FROM "NewsList" ${filterCondition}`;
 
-    if (title) {
-      // const condition = title ? { news_title: { [Op.iLike]: `%${title}%` } } : null;
-      const condition = { news_title: { [Op.iLike]: `%${title}%` } };
-      requestedNews = await NewsList.findAll({ where: condition });
-      res.status(200).send(requestedNews);
-    }
+    // if (title) {
+    //   // const condition = title ? { news_title: { [Op.iLike]: `%${title}%` } } : null;
+    //   const condition = { news_title: { [Op.iLike]: `%${title}%` } };
+    //   requestedNews = await NewsList.findAll({ where: condition });
+    //   res.status(200).send(requestedNews);
+    // }
 
     // { type: QueryTypes.SELECT } HELPED TO GET NEWS WITHOUT USELESS DATA
     // google raw queries
     // https://sequelize.org/docs/v6/core-concepts/raw-queries/
     requestedNews = await sequelize.query(query, { type: QueryTypes.SELECT });
+    const count = await sequelize.query(countQuery, {
+      type: QueryTypes.SELECT,
+    });
 
     // Getting the list of all news
     // count = the length of listOfAllNews array
     let listOfAllNews = await NewsList.findAll();
-    // let count = listOfAllNews.length;
 
-    let count = await NewsList.count();
-    console.log('count: ', count);
+    // let count = await NewsList.count();
+    console.log('----------------------count: ', count[0].count);
 
-    result = { page, limit, offset, count, requestedNews };
+    result = { page, limit, offset, count: count[0].count, requestedNews };
     // console.log('result from findByTitle: ', result);
 
     res.status(200).send(result);
@@ -100,7 +117,7 @@ const findByTitle = async (req, res) => {
     // 4 OPTION
     // res.status(200).send(await NewsList.findAll({ where: req.query.title ? { news_title: { [Op.iLike]: `%${req.query.title}%` } } : null }));
   } catch (e) {
-    console.log('err', e);
+    // console.log('err', e);
   }
 };
 
